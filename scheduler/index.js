bec14535fec1d933d2cb031e0e588552e24117c3
lviss <lviss@users.noreply.github.com>
2020-08-06 00:31:47 -0700

    Fixed it so you don't get repeat notifications while the door stays open

--- scheduler/index.js
+++ scheduler/index.js
@@ -4,6 +4,10 @@ var mqtt = require('mqtt');
 var mqttclient  = mqtt.connect(config.mqtt_server);
 
 let timers = {};
+// as far as i can tell there is no way to see if a timer has
+// been cleared or is still scheduled, so we're going to keep 
+// track of that separately.
+let isTimerScheduled = {};
 
 mqttclient.on('connect', function () {
   mqttclient.subscribe('devices/garage_door_sensor2');
@@ -13,19 +17,23 @@ mqttclient.on('connect', function () {
 mqttclient.on('message', function (topic, message) {
   var data = JSON.parse(message.toString());
   if (data.open) {
-    console.log('detected door opening, waiting...');
-    // clear any existing timer for this door
-    clearTimeout(timers[topic]);
-    // set the timer
-    timers[topic] = setTimeout(notify_me, 1000 * 60 * 10);
+    console.log('[' + new Date().toUTCString() + '] ', 'detected', topic, 'opening');
+    if (!isTimerScheduled[topic]) {
+      console.log('[' + new Date().toUTCString() + '] ', 'waiting 10 mins to alert...');
+      // set the timer
+      timers[topic] = setTimeout(notify_me, 1000 * 60 * 10);
+      isTimerScheduled[topic] = true;
+    }
   } else {
     // clear the timer
     clearTimeout(timers[topic]);
-    console.log('detected door closing, cancelling timer...');
+    isTimerScheduled[topic] = false;
+    console.log('[' + new Date().toUTCString() + '] ', 'detected', topic, 'closing, cancelling timer...');
   }
 });
 
 function notify_me() {
   let message = `Warning: The garage has been open for 10 minutes.`;
+  console.log('[' + new Date().toUTCString() + '] ', 'sending warning:', message);
   mqttclient.publish('notify/me', JSON.stringify({ message: message }));
 }
