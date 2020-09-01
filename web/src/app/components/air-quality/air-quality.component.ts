259289a9ecb3c2f8161abb7962dc62d57859a1ce
Lane <lviss@users.noreply.github.com>
2020-08-31 17:42:43 -0700

    Refresh air quality every 10 mins

--- web/src/app/components/air-quality/air-quality.component.ts
+++ web/src/app/components/air-quality/air-quality.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AirQualityService } from '../../services/air-quality.service';
 import aqi from 'aqi-us';
+import {interval} from "rxjs/internal/observable/interval";
+import {startWith, switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-air-quality',
@@ -18,7 +20,10 @@ export class AirQualityComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.airQualityService.get().subscribe((res : any) => {
+    interval(1000 * 60 * 10).pipe( // every 10 mins
+      startWith(0),
+      switchMap(() => this.airQualityService.get())
+    ).subscribe((res : any) => {
       this.airQuality = res;
       this.aqi = aqi.pm25(res.results[0].PM2_5Value);
       if (this.aqi < 50)
